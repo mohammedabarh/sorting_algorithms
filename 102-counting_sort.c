e927b6a1ef0e633ef77bb4d2f3eaa5d0523974c0
mohammedabarh <abmohammed606@gmail.com>
2024-10-29 20:56:33 -0700

    Added quick sort (Hoare) and deck sorting implementations

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,70 +9,65 @@
  */
 int get_max(int *array, size_t size)
 {
-	size_t i;
-	int max;
+        size_t i;
+        int max;
 
-	max = array[0];
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-			max = array[i];
-	}
-	return (max);
+        max = array[0];
+        for (i = 1; i < size; i++)
+        {
+                if (array[i] > max)
+                        max = array[i];
+        }
+        return (max);
 }
 
 /**
  * counting_sort - Sorts array of integers using Counting sort algorithm
  * @array: Array to sort
  * @size: Size of array
+ * Description: Prints the counting array after setting it up
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count, *output;
-	int max, i;
-	size_t j;
+        int *count, *output;
+        int max, i;
+        size_t j;
 
-	if (!array || size < 2)
-		return;
+        if (!array || size < 2)
+                return;
 
-	max = get_max(array, size);
-	count = malloc(sizeof(int) * (max + 1));
-	if (!count)
-		return;
+        max = get_max(array, size);
+        count = malloc(sizeof(int) * (max + 1));
+        if (!count)
+                return;
 
-	output = malloc(sizeof(int) * size);
-	if (!output)
-	{
-		free(count);
-		return;
-	}
+        output = malloc(sizeof(int) * size);
+        if (!output)
+        {
+                free(count);
+                return;
+        }
 
-	/* Initialize count array with zeros */
-	for (i = 0; i <= max; i++)
-		count[i] = 0;
+        for (i = 0; i <= max; i++)
+                count[i] = 0;
 
-	/* Store count of each element */
-	for (j = 0; j < size; j++)
-		count[array[j]]++;
+        for (j = 0; j < size; j++)
+                count[array[j]]++;
 
-	/* Update count[i] to store actual position */
-	for (i = 1; i <= max; i++)
-		count[i] += count[i - 1];
+        for (i = 1; i <= max; i++)
+                count[i] += count[i - 1];
 
-	/* Print counting array */
-	print_array(count, max + 1);
+        print_array(count, max + 1);
 
-	/* Build output array */
-	for (i = size - 1; i >= 0; i--)
-	{
-		output[count[array[i]] - 1] = array[i];
-		count[array[i]]--;
-	}
+        for (i = size - 1; i >= 0; i--)
+        {
+                output[count[array[i]] - 1] = array[i];
+                count[array[i]]--;
+        }
 
-	/* Copy output array to original array */
-	for (j = 0; j < size; j++)
-		array[j] = output[j];
+        for (j = 0; j < size; j++)
+                array[j] = output[j];
 
-	free(count);
-	free(output);
+        free(count);
+        free(output);
 }

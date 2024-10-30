e5bbe75c5863bdf2e67f8377658c60ae9daecf97
mohammedabarh <abmohammed606@gmail.com>
2024-10-29 20:42:27 -0700

    Added quick sort (Hoare) and deck sorting implementations

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,13 +8,15 @@
  */
 int get_max(int *array, size_t size)
 {
-        int max = array[0];
-        size_t i;
+    int max = array[0];
+    size_t i;
 
-        for (i = 1; i < size; i++)
-                if (array[i] > max)
-                        max = array[i];
-        return (max);
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+    }
+    return (max);
 }
 
 /**
@@ -24,48 +26,48 @@ int get_max(int *array, size_t size)
  */
 void counting_sort(int *array, size_t size)
 {
-        int *counting_array, *output, max, i;
+    int *counting_array, *output, max, i;
 
-        if (!array || size < 2)
-                return;
+    if (!array || size < 2)
+        return;
 
-        max = get_max(array, size);
-        counting_array = malloc(sizeof(int) * (max + 1));
-        if (!counting_array)
-                return;
+    max = get_max(array, size);
+    counting_array = malloc(sizeof(int) * (max + 1));
+    if (!counting_array)
+        return;
 
-        output = malloc(sizeof(int) * size);
-        if (!output)
-        {
-                free(counting_array);
-                return;
-        }
-
-        /* Initialize counting array */
-        for (i = 0; i <= max; i++)
-                counting_array[i] = 0;
-
-        /* Count occurrences */
-        for (i = 0; i < (int)size; i++)
-                counting_array[array[i]]++;
-
-        /* Calculate cumulative count */
-        for (i = 1; i <= max; i++)
-                counting_array[i] += counting_array[i - 1];
-
-        print_array(counting_array, max + 1);
-
-        /* Build output array */
-        for (i = size - 1; i >= 0; i--)
-        {
-                output[counting_array[array[i]] - 1] = array[i];
-                counting_array[array[i]]--;
-        }
-
-        /* Copy output array to original array */
-        for (i = 0; i < (int)size; i++)
-                array[i] = output[i];
-
+    output = malloc(sizeof(int) * size);
+    if (!output)
+    {
         free(counting_array);
-        free(output);
+        return;
+    }
+
+    /* Initialize counting array */
+    for (i = 0; i <= max; i++)
+        counting_array[i] = 0;
+
+    /* Count occurrences */
+    for (i = 0; i < (int)size; i++)
+        counting_array[array[i]]++;
+
+    /* Calculate cumulative count */
+    for (i = 1; i <= max; i++)
+        counting_array[i] += counting_array[i - 1];
+
+    print_array(counting_array, max + 1);
+
+    /* Build output array */
+    for (i = size - 1; i >= 0; i--)
+    {
+        output[counting_array[array[i]] - 1] = array[i];
+        counting_array[array[i]]--;
+    }
+
+    /* Copy output array to original array */
+    for (i = 0; i < (int)size; i++)
+        array[i] = output[i];
+
+    free(counting_array);
+    free(output);
 }

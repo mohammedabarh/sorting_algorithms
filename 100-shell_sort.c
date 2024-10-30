4220da4c05ce8c619b45d51ad10133b759c74ea9
mohammedabarh <abmohammed606@gmail.com>
2024-10-29 20:13:56 -0700

    Added quick sort (Hoare) and deck sorting implementations

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,32 +2,36 @@
 
 /**
  * shell_sort - Sorts an array using Shell sort algorithm with Knuth sequence
- * @array: Array to sort
+ * @array: Array to be sorted
  * @size: Size of the array
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int temp;
+	size_t gap = 1, i, j;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+	/* Calculate initial gap using Knuth sequence */
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            temp = array[i];
-            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-            {
-                array[j] = array[j - gap];
-            }
-            array[j] = temp;
-        }
-        print_array(array, size);
-        gap = (gap - 1) / 3;
-    }
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
 }

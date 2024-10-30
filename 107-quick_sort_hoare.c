5ec973a54199d7cf81263551d0b3c6f8bbc26300
mohammedabarh <abmohammed606@gmail.com>
2024-10-29 20:37:03 -0700

    Added quick sort (Hoare) and deck sorting implementations

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,72 +1,85 @@
 #include "sort.h"
+
 /**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
+ * swap - Exchanges the positions of two elements in an array
+ * @array: The input array to modify
+ * @item1: Index of the first element to swap
+ * @item2: Index of the second element to swap
+ *
+ * Description: Performs an in-place swap of two array elements
+ */
 void swap(int *array, ssize_t item1, ssize_t item2)
 {
-	int tmp;
+        int tmp;
 
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
+        tmp = array[item1];
+        array[item1] = array[item2];
+        array[item2] = tmp;
 }
+
 /**
- *hoare_partition - hoare partition sorting scheme implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: size array
- *Return: return the position of the last element sorted
+ * hoare_partition - Implements the Hoare partition scheme for quicksort
+ * @array: The array to be partitioned
+ * @first: Starting index of the partition
+ * @last: Ending index of the partition
+ * @size: Total size of the array
+ * Return: Position of the partition index
+ *
+ * Description: Uses the last element as pivot and partitions the array
+ * into two sub-arrays - elements less than pivot and elements greater than pivot
  */
 int hoare_partition(int *array, int first, int last, int size)
 {
-	int current = first - 1, finder = last + 1;
-	int pivot = array[last];
+        int current = first - 1, finder = last + 1;
+        int pivot = array[last];
 
-	while (1)
-	{
-
-		do {
-			current++;
-		} while (array[current] < pivot);
-		do {
-			finder--;
-		} while (array[finder] > pivot);
-		if (current >= finder)
-			return (current);
-		swap(array, current, finder);
-		print_array(array, size);
-	}
+        while (1)
+        {
+                do {
+                        current++;
+                } while (array[current] < pivot);
+                do {
+                        finder--;
+                } while (array[finder] > pivot);
+                if (current >= finder)
+                        return (current);
+                swap(array, current, finder);
+                print_array(array, size);
+        }
 }
+
 /**
- *qs - qucksort algorithm implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: array size
+ * qs - Recursive implementation of the quicksort algorithm
+ * @array: The array to be sorted
+ * @first: Starting index of the current partition
+ * @last: Ending index of the current partition
+ * @size: Total size of the array
+ *
+ * Description: Recursively sorts the array using Hoare's partition scheme
  */
 void qs(int *array, ssize_t first, ssize_t last, int size)
 {
-	ssize_t position = 0;
+        ssize_t position = 0;
 
-	if (first < last)
-	{
-		position = hoare_partition(array, first, last, size);
-		qs(array, first, position - 1, size);
-		qs(array, position, last, size);
-	}
+        if (first < last)
+        {
+                position = hoare_partition(array, first, last, size);
+                qs(array, first, position - 1, size);
+                qs(array, position, last, size);
+        }
 }
+
 /**
- *quick_sort_hoare - prepare the terrain to quicksort algorithm
- *@array: array
- *@size: array size
+ * quick_sort_hoare - Entry point for the Hoare quicksort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ *
+ * Description: Initializes the quicksort process using Hoare's partition scheme
+ * Returns immediately if array is NULL or has less than 2 elements
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (!array || size < 2)
-		return;
-	qs(array, 0, size - 1, size);
+        if (!array || size < 2)
+                return;
+        qs(array, 0, size - 1, size);
 }
